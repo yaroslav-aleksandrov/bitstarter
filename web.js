bb143c4642be331ddbff315428d1faa8791c5f27
Yaroslav Aleksandrov <Yaroslav.Aleksandrov@gmail.com>
2013-07-10 21:13:18 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs =  require('fs')
+var buffer = new Buffer(200)
 
 var app = express.createServer(express.logger());
 
+buffer = fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
